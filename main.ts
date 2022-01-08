485398a67537a4b173df742584ae5bbd1a5a29da
LeviDog99 <83308663+LeviDog99@users.noreply.github.com>
2022-01-07 19:06:06 -0500

    click a to play music

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 let Sound = 0
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 function doSomething () {
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
